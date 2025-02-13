24681abdb191356f754b60b8b5f8a513920f50af
Yogesh Kushwah <130173369+Yogeshleo8077@users.noreply.github.com>
2025-02-14 00:03:58 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `I am a passionate Web Developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React, Tailwind CSS and Bootstrap, as well as back-end technologies like Node.js, Express.js and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Node.js, Express.js, Tailwind CSS, Bootstrap and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile Web Developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Node.js, Express.js, Tailwind CSS, Bootstrap and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
@@ -44,7 +44,7 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional food delivery website with features like add food items, food cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB", "Express"],
   },
   {
     title: "Tic Tac Toe",
