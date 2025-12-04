a1c854a7b3251661dcf2f673e07ce6c5b1e061aa
Yogeshleo8077 <yogeshleo420@gmail.com>
2025-12-04 12:32:14 +0530

    Added profile photo

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/profile.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
